fd82f84dcc01bf4a9553d7ff1476f150a3379e60
skyinsky <skyinsky@yeah.net>
2014-09-07 21:01:25 +0800

    update

--- test/t_mysql.cc
+++ test/t_mysql.cc
@@ -9,7 +9,7 @@ int main(int argc, char *argv[]) {
 	MysqlWrapper *con;
 	con = new MysqlWrapper;
 	con->Connect("127.0.0.1", "root", "123456");
-	con->SelectQuery("select * from holy.student");
+	con->SelectQuery("select * from 用户");
 	while (char** r = con->FetchRow())
 		printf("%s\t%s\t%s\n", r[0], r[1], r[2]);
 	return 0;
