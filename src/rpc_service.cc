00535ef10b460f978b27fa221ea027c39b015c61
skyinsky <skyinsky@yeah.net>
2014-10-27 00:42:31 +0800

    update

---
+++ src/rpc_service.cc
@@ -0,0 +1,75 @@
+#include <stdlib.h>
+#include "rpc_service.h"
+
+namespace cobaya {
+
+void RpcServiceImpl::GetAdminInfo(RpcController *ctl,
+				  const MsgAdminReq *req,
+				  MsgAdminRsp *rsp, Closure *done)
+{
+
+}
+
+void RpcServiceImpl::ControlSystem(RpcController *ctl,
+				   const MsgMonitorReq *req,
+				   MsgMonitorRsp *rsp, Closure *done)
+{
+
+}
+
+void RpcServiceImpl::GetClientInfo(RpcController *ctl,
+				   const MsgClientReq *req,
+				   MsgClientRsp *rsp, Closure *done)
+{
+
+}
+
+void RpcServiceImpl::AppendInfo(RpcController *ctl,
+				const MsgDiscoveryReq *req,
+				MsgDiscoveryRsp *rsp, Closure *done)
+{
+
+}
+
+void RpcServiceImpl::RegisterUser(RpcController *ctl,
+				  const MsgRegUserReq *req,
+				  MsgRegUserRsp *rsp, Closure *done)
+{
+
+}
+
+void RpcServiceImpl::LoginSystem(RpcController *ctl,
+				 const MsgLoginReq *req,
+				 MsgLoginRsp *rsp, Closure *done)
+{
+
+}
+
+void RpcServiceImpl::LogoutSystem(RpcController *ctl,
+				  const MsgLogoutReq *req,
+				  MsgLogoutRsp *rsp, Closure *done)
+{
+
+}
+
+void RpcServiceImpl::StartFlow(RpcController *ctl,
+			       const MsgStartReq *req,
+			       MsgStartRsp *rsp, Closure *done)
+{
+
+}
+
+void RpcServiceImpl::StopFlow(RpcController *ctl,
+			      const MsgStopReq *req,
+			      MsgStopRsp *rsp, Closure *done)
+{
+
+}
+
+
+
+
+
+
+
+} // namespace cobaya
---
+++ src/rpc_service.h
@@ -0,0 +1,43 @@
+#ifndef _RPC_SERVICE_H
+#define _RPC_SERVICE_H
+
+#include <librcfproto/RCFProto.hpp>
+#include "protocol/cobaya.pb.h"
+
+namespace cobaya {
+
+using namespace google::protobuf;
+
+class RpcServiceImpl : public RpcService {
+public:
+	RpcServiceImpl() { }
+	~RpcServiceImpl() { }
+
+	/*  admin operations */
+	void GetAdminInfo(RpcController *ctl, const MsgAdminReq *req,
+			  MsgAdminRsp *rsp, Closure *done);
+	void ControlSystem(RpcController *ctl, const MsgMonitorReq *req,
+			   MsgMonitorRsp *rsp, Closure *done);
+
+	/* client operations */
+	void GetClientInfo(RpcController *ctl, const MsgClientReq *req,
+			   MsgClientRsp *rsp, Closure *done);
+	void AppendInfo(RpcController *ctl, const MsgDiscoveryReq *req,
+			MsgDiscoveryRsp *rsp, Closure *done);
+	void RegisterUser(RpcController *ctl, const MsgRegUserReq *req,
+			  MsgRegUserRsp *rsp, Closure *done);
+	void LoginSystem(RpcController *ctl, const MsgLoginReq *req,
+			 MsgLoginRsp *rsp, Closure *done);
+	void LogoutSystem(RpcController *ctl, const MsgLogoutReq *req,
+			  MsgLogoutRsp *rsp, Closure *done);
+
+	/* his operations */
+	void StartFlow(RpcController *ctl, const MsgStartReq *req,
+		       MsgStartRsp *rsp, Closure *done);
+	void StopFlow(RpcController *ctl, const MsgStopReq *req,
+		      MsgStopRsp *rsp, Closure *done);
+};
+
+} // namespace cobaya
+
+#endif /* _RPC_SERVICE_H */
